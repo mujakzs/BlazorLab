526b348f4b271ee214ec191d04ac41b5b0d60cb2
Andrian <andriangultiano14@gmail.com>
2025-09-20 22:02:38 +0800

    Data binding with checkbox

--- BlazorLab/Models/Product.cs
+++ BlazorLab/Models/Product.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public string Name { get; set; }
 
-        public int Price { get; set; }
+        public double Price { get; set; }
         public bool isActive { get; set; }
     }
 }
