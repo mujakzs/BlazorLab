cfbc4fcb17e85f40d6b4954580626a742fbf25dc
Andrian <andriangultiano14@gmail.com>
2025-09-21 23:40:31 +0800

    Adding Product List

---
+++ BlazorLab/Models/Employee_prop.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorLab.Models
+{
+    public class Employee_prop
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string Team { get; set; }
+
+        public string Strength { get; set; }
+    }
+}
